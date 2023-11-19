f2017f272bfcf1a889766852a99e7d11043758e1
Gilad Weinberger <giladwe21@gmail.com>
2023-11-19 19:14:04 +0200

    add route to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router basename={import.meta.env.BASE_URL}>
+      <App />
+    </Router>
+  </React.StrictMode>
+);

a9351018bc180d50796362e4f63be170b5ff8abd
Gilad Weinberger <giladwe21@gmail.com>
2023-11-19 19:08:03 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Me from './components/Me'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Me from "./components/Me";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Me />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </>
-  )
-}
+    <Router basename="/portfolio">
+      <Route path="/">
+        <Navbar />
+        <Me />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </Route>
+    </Router>
+  );
+};
 
-export default App
+export default App;

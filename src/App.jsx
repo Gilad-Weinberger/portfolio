16539c65cebe6b2006c895a4e29eef0e92c771db
Gilad Weinberger <giladwe21@gmail.com>
2023-11-19 19:10:47 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Me from "./components/Me";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react'
+import Navbar from './components/Navbar'
+import Me from './components/Me'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Router basename="/portfolio">
-      <Switch>
-        <Route path="/">
-          <Navbar />
-          <Me />
-          <About />
-          <Projects />
-          <Contact />
-          <Footer />
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
+    <>
+      <Navbar />
+      <Me />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
 
-export default App;
+export default App
